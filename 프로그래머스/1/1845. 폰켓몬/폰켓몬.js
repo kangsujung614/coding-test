e6dbca38a18c06ef6472580f167a5deb6006fcf9
kangsujung614 <sjkang614@gmail.com>
2024-09-14 13:21:22 +0900

    [level 1] Title: 폰켓몬, Time: 3.29 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,11 +1,10 @@
+// 해시로 풀이
 function solution(nums) {
-    let answer = 0;
-    const ponkemon = nums.length / 2;
-    const set = new Set(nums);
-    if(set.size > ponkemon){
-        answer = ponkemon;
-    } else {
-        answer = set.size
-    }
-    return answer;
+    const hash = nums.reduce((acc, cur) => {
+        acc[cur] ? acc[cur]++ : acc[cur] = 1;
+        return acc;
+    }, {}) // 초깃값이 {}(객체)이므로 acc가 객체의 식별자처럼 작동
+    const kind = Object.keys(hash).length; // 폰켓몬 종류
+    const get = nums.length / 2; // 가져갈 수 있는 폰켓몬 갯수
+    return kind > get ? get : kind;
 }
